59405ce50047cfb4a34dc1f1b34eb56044fbf195
katherineobide <70817641+obidekatherine@users.noreply.github.com>
2022-04-11 14:52:12 +0100

    update

---
+++ module1/content/result-async.js
@@ -0,0 +1,26 @@
+//async
+const {readFile, writeFile} = require('fs'); // reading two files or const fs = require('fs');
+//callback is called when we are done
+
+readFile('./content/first.txt', 'utf8', (err, result) => {
+    if(err) {
+        console.log(err)
+        return
+    }
+    const firstTxt = result;
+    readFile('./content/second.txt', 'utf8',(err,result)=> {
+        if(err) {
+            console.log(err)
+            return
+        }
+        const second = result;
+        writeFile('./content/result-async.txt', `Hello World! : ${firstTxt}, ${second}`, (err, result) => {
+            if(err) {
+                console.log(err)
+                return
+            }
+            console.log(result)
+        })   
+    })
+})
+
