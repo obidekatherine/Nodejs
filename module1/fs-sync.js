14a2562c8cec59b270d9dc9d108ba7230e6c0d50
katherineobide <70817641+obidekatherine@users.noreply.github.com>
2022-04-11 15:26:26 +0100

    update

--- module1/fs-sync.js
+++ module1/fs-sync.js
@@ -1,6 +1,8 @@
 //readfilesync
 const {readFileSync, writeFileSync} = require('fs'); // reading two files or const fs = require('fs');
 
+console.log('Start');
+
 const first = readFileSync('./content/first.txt', 'utf-8');
 const second = readFileSync('./content/second.txt', 'utf-8');
 
@@ -9,3 +11,6 @@ const second = readFileSync('./content/second.txt', 'utf-8');
 writeFileSync('./content/result-sync.txt', 
 `Here is it : ${first}, ${second}`, {flag: 'a'} //flag to great a new  value
 ) //created a file that did'nt exist
+
+console.log('End');
+console.log('Start again');
