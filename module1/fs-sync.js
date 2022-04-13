528809f3b3be8ac23bfdcd47d553e46bfb36875e
katherineobide <70817641+obidekatherine@users.noreply.github.com>
2022-04-13 18:19:43 +0100

    update

--- module1/fs-sync.js
+++ module1/fs-sync.js
@@ -14,3 +14,31 @@ writeFileSync('./content/result-sync.txt',
 
 console.log('End');
 console.log('Start again');
+
+
+//Blocking code
+
+const http = require('http')
+
+const server = http.createServer((req, res)=> {
+  if(req.url === '/') {
+      res.end('Home Page')
+  } 
+  if(req.url === '/about') {
+//BLOCKING CODE
+
+for(let i = 0; i < 1000; i++){
+    for(let j = 0; j < 1000; j++) {
+        console.log(`${i} ${j}`);
+    }
+}
+    res.end('about Page')
+}
+
+res.end('Error Page')
+})
+
+
+server.listen(5000, () => {
+    console.log('server listening on port  5000....');
+})
